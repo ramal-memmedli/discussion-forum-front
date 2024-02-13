5ce3412a50c4017b0dedef96e530cdac8cf069f0
ramal-memmedli <memmedliramal00@gmail.com>
2024-02-13 19:56:21 +0400

    Left sidebar menu redesigned

--- assets/script/search.js
+++ assets/script/search.js
@@ -1,6 +1,10 @@
 var searchBtn = document.getElementById('searchBar');
 var searchArea = document.getElementById('searchResult');
 
-searchBtn.addEventListener("focus", function(){
-    searchArea.classList.toggle("search-area-opened");
+searchBtn.addEventListener("focusin", function(){
+    searchArea.classList.add("search-area-opened");
 });
+
+searchBtn.addEventListener("focusout", function (){
+    searchArea.classList.remove("search-area-opened");
+})
