4dbb0db2d01b3dbac257e950ddf0041e6a6879f0
ramal-memmedli <memmedliramal00@gmail.com>
2024-02-14 21:03:12 +0400

    Redesign work

--- assets/script/userNav.js
+++ assets/script/userNav.js
@@ -1,8 +1,7 @@
-var headerUsers = document.querySelectorAll('.header-user');
-var userNav = document.getElementById('userNav');
 
-for (const headerUser of headerUsers) {
-    headerUser.addEventListener("click", function(){
-        userNav.classList.toggle('user-nav-opened');
-    });
-}
+
+$(document).ready(function(){
+    $(".header-user").click(function(){
+        $("#userNav").fadeToggle('fast')
+    })
+});
