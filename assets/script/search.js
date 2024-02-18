f7afd32b15da70f39626577447c0f9bad5f2234d
ramal-memmedli <memmedliramal00@gmail.com>
2024-02-18 04:10:32 +0400

    Overall redesign work

--- assets/script/search.js
+++ assets/script/search.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     var searchBtn = $('#searchBar');
     var searchArea = $('#searchResult');
 
-    searchBtn.addEventListener("focusin", function () {
-        searchArea.classList.add("search-area-opened");
-    });
+    searchBtn.on("focusin", function() {
+        searchArea.addClass("search-area-opened")
+    })
 
-    searchBtn.addEventListener("focusout", function () {
-        searchArea.classList.remove("search-area-opened");
+    searchBtn.on("focusout", function() {
+        searchArea.removeClass("search-area-opened")
     })
 })
