463b73bc1e9a7e90fcd2182c07277f6e07904a08
ramal-memmedli <memmedliramal00@gmail.com>
2024-03-17 22:40:46 +0400

    redesign

--- assets/script/darkMode.js
+++ assets/script/darkMode.js
@@ -1,15 +1,15 @@
-var switchBtn = document.getElementById("switch");
-var switchBox = document.querySelector("#switch input");
+// var switchBtn = document.getElementById("switch");
+// var switchBox = document.querySelector("#switch input");
 
-switchBtn.addEventListener("change", () => {
-    enableDarkMode();
-});
+// switchBtn.addEventListener("change", () => {
+//     enableDarkMode();
+// });
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    enableDarkMode();
-    switchBox.checked = true;
-}
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     enableDarkMode();
+//     switchBox.checked = true;
+// }
 
-function enableDarkMode() {
-    document.body.classList.toggle('dark-mode');
-}
+// function enableDarkMode() {
+//     document.body.classList.toggle('dark-mode');
+// }
